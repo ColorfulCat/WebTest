7b6f7ece957201e0721b7a02e5bddabcb2882757
封羽 <devislee.lb@alibaba-inc.com>
2016-04-19 11:50:02 +0800

    add forum

--- scripts/VSAndroidView.js
+++ scripts/VSAndroidView.js
@@ -2,7 +2,7 @@ function VSAndroidView() {
 	LinearLayout.apply(this);
 	var layoutParam = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
 	var mWebView = new WebView();
-	mWebView.loadUrl("http://androidcat.com/");//http://133.130.120.73:9000/
+	mWebView.loadUrl("http://133.130.120.73:9000/");//http://133.130.120.73:9000/
 	this.addView(mWebView, layoutParam);
 	this.requestLayout();
 }
