5c3c1991fb6565a3bf4289e6ae26618c3fc22f25
Devis <598252892@qq.com>
2016-06-06 12:48:24 +0800

    s

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 $( document ).ready(function(){
 	
-	$(".button-collapse").sideNav();
+	$(".button-collapse").sideNav({
+      menuWidth: 240, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    });
 	
 	$('.dropdown-button').dropdown({
       inDuration: 300,
