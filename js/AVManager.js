f2ef763e3fa21fee4af48952f823a10f66f4d890
Devis <598252892@qq.com>
2016-06-14 22:49:56 +0800

    av

--- js/AVManager.js
+++ js/AVManager.js
@@ -1,75 +1,75 @@
 var appId = 'MPPk3pQdp6kIl6tAJI2HWuDF';
 var appKey = 'TsuMnwADjcIkD1po2kKn8U7F';
 AV.initialize(appId, appKey);
-   // 实例化统计分析功能
+// 实例化统计分析功能
 var analytics = AV.analytics({
-    // 设置 AppId
-    appId: appId,
-    // 设置 AppKey
-    appKey: appKey,
-    // 你当前应用或者想要指定的版本号（自定义）
-    version: '3.0.0',
-    // 你当前应用的渠道或者你想指定的渠道（自定义）
-    channel: 'develop'
+	// 设置 AppId
+	appId: appId,
+	// 设置 AppKey
+	appKey: appKey,
+	// 你当前应用或者想要指定的版本号（自定义）
+	version: '3.0.0',
+	// 你当前应用的渠道或者你想指定的渠道（自定义）
+	channel: 'develop'
 });
 
 analytics.send({
-    // 事件名称
-    event: 'view_in',
-//
-//  // 事件属性，任意数据
-//  attr: {
-//      testa: 123,
-//      testb: 'abc'
-//  },
-    // 该事件持续时间（毫秒）
-    duration: 6000}, function(result) {
-    if (result) {
-        console.log('成功统计到用户！');
-    }
+	// 事件名称
+	event: 'view_in',
+	//
+	//  // 事件属性，任意数据
+	//  attr: {
+	//      testa: 123,
+	//      testb: 'abc'
+	//  },
+	// 该事件持续时间（毫秒）
+	duration: 6000
+}, function(result) {
+	if (result) {
+		console.log('成功统计到用户！');
+	}
 });
 
-function updateCatHit(objectId){
+function updateCatHit(objectId) {
 	var Post = AV.Object.extend('Cat');
 	var query = new AV.Query(Post);
 	query.get(objectId).then(function(post) {
 		console.log('success when updateHit');
-	    // 成功获得实例
-	    post.increment('hit');
+		// 成功获得实例
+		post.increment('hit');
 		post.save().then(function() {
 			console.log('success when updateHit increment');
-		  // 保存成功
+			// 保存成功
 		}, function(error) {
 			console.log('error when updateHit increment ' + error.code + ' ' + error.message);
-		  // 失败
+			// 失败
 		});
 	}, function(error) {
 		console.log('error when updateHit ' + error.code + ' ' + error.message);
-	  // 失败了
+		// 失败了
 	});
 }
 
-function updateCatMenuHit(objectId){
-	console.log('updateCatMenuHit ' +objectId);
+function updateCatMenuHit(objectId) {
+	console.log('updateCatMenuHit ' + objectId);
 	var Post = AV.Object.extend('Sort');
 	var query = new AV.Query(Post);
 	query.get(objectId).then(function(post) {
 		console.log('success when updateCatMenuHit');
-	    // 成功获得实例
-	    post.increment('hit');
+		// 成功获得实例
+		post.increment('hit');
 		post.save().then(function() {
 			console.log('success when updateCatMenuHit increment');
-		  // 保存成功
+			// 保存成功
 		}, function(error) {
-			console.log('error when updateCatMenuHit increment ' +  error.code + ' ' + error.message);
-		  // 失败
+			console.log('error when updateCatMenuHit increment ' + error.code + ' ' + error.message);
+			// 失败
 		});
 	}, function(error) {
-		console.log('error when updateCatMenuHit '  + error.code + ' ' + error.message);
-	  // 失败了
+		console.log('error when updateCatMenuHit ' + error.code + ' ' + error.message);
+		// 失败了
 	});
 }
-
 
 //
 //function addCat(site){
@@ -94,7 +94,8 @@ function queryMenus() {
 	var query = new AV.Query('Sort');
 	query.addAscending('order');
 	query.find().then(function(results) {
-		mMenus.clear();
+		console.log(" === " + results.length);
+		mMenus = [];
 		for (var i = 0; i < results.length; i++) {
 			var catMenuItem = {};
 			catMenuItem.index = i;
@@ -108,11 +109,23 @@ function queryMenus() {
 			}
 			mMenus.push(catMenuItem);
 		}
-//		updateMenus();
-//		queryCats(mMenus[0]);
+		updateMenus();
+		console.log(" === end");
+		//		queryCats(mMenus[0]);
 	}, function(error) {
 		alert('Error: ' + error.code + ' ' + error.message);
 	});
 }
-queryMenus();
 
+function updateMenus() {
+
+	for (var i = 0; i < mMenus.length; i++) {
+		console.log(" === " + i);
+		var msg = '<a href="#!" id="menu0" class="center-align collection-item waves-effect">' + mMenus[i].title + '</a>';;
+
+		$("#menus").append(msg);
+	}
+
+}
+
+queryMenus();
